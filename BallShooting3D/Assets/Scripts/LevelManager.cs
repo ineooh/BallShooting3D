cc2c9ac1a0dcdfac77e7a4ba003fcc21ce7bb0cf
Vũ Đức Trung <vuductrung14112001@gmail.com>
2022-12-04 14:27:45 +0700

    [Coding] Level Manager

--- BallShooting3D/Assets/Scripts/LevelManager.cs
+++ BallShooting3D/Assets/Scripts/LevelManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static LevelManager Instance;
+
+    [SerializeField] private GameObject _loaderCanvas;
+    [SerializeField] private Slider _progressBar;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public async void LoadScene(string sceneName)
     {
-        
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+
+        scene.allowSceneActivation = false;
+
+        _loaderCanvas.SetActive(true);
+
+        do
+        {
+            await Task.Delay(100);
+            _progressBar.value = scene.progress;
+        } while (scene.progress < 0.9f);
+
+        await Task.Delay(1000);
+
+        scene.allowSceneActivation = true;
+        _loaderCanvas.SetActive(false);
     }
 }
