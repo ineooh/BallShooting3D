6e0ae378f651fffe64a7daccf5118b7ac2776701
Vũ Đức Trung <vuductrung14112001@gmail.com>
2022-12-12 15:07:29 +0700

    [Fixing] A lot of bugs :))))

--- BallShooting3D/Assets/Scripts/MainCharacter.cs
+++ BallShooting3D/Assets/Scripts/MainCharacter.cs
@@ -11,5 +11,9 @@ public class MainCharacter : MonoBehaviour
     }
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            LevelManager.Instance.LoadScene("LevelScene");
+        }
     }
 }
